6970cf5b9d188039c5159727887ca710e910ff34
Macy Hong <macyhong@gmail.com>
2022-11-01 03:19:12 +0000

    When space key is pressed, move the car every 16 milliseconds #3

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var $car = document.querySelector('img');
 
+// Turn the car
 document.addEventListener('keydown', turn);
-
 function turn(e) {
   if (e.key === 'ArrowDown') {
     $car.style.transform = 'rotate(270deg)';
@@ -11,5 +11,22 @@ function turn(e) {
     $car.style.transform = 'rotate(90deg)';
   } else if (e.key === 'ArrowRight') {
     $car.style.transform = 'rotate(180deg)';
+  }
+}
+
+// Data
+var coordX = $car.offsetLeft;
+// var coordY = $car.offsetTop;
+
+// Start the car
+document.addEventListener('keydown', go);
+function go(e) {
+  if (e.key === ' ') {
+    // console.log(coordX);
+    setInterval(() => {
+      coordX += 20;
+      $car.style.left = `${coordX}px`;
+    }, 16);
+
   }
 }
